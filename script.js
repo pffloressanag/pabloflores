c2046c5dbd6b543abcd8eed52db034558e3e9492
PFFlores <pfflores.sana@gmail.com>
2026-03-26 18:10:32 -0300

    Mi portafolio web

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 function toggleMenu(){
 
-const menu=document.getElementById("menu")
+const menu = document.getElementById("menu");
+const btn = document.querySelector(".menu-btn");
 
-menu.style.display=
-menu.style.display==="flex"?"none":"flex"
+menu.style.display =
+menu.style.display === "flex" ? "none" : "flex";
+
+btn.classList.toggle("active");
 
 }
 
@@ -62,3 +65,80 @@ function revealSections() {
 }
 
 window.addEventListener("scroll", revealSections);
+
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll("nav ul li a");
+
+window.addEventListener("scroll", () => {
+
+let current = "";
+
+sections.forEach(section => {
+
+const sectionTop = section.offsetTop;
+const sectionHeight = section.clientHeight;
+
+if (pageYOffset >= sectionTop - 200) {
+current = section.getAttribute("id");
+}
+
+});
+
+navLinks.forEach(link => {
+
+link.classList.remove("active");
+
+if (link.getAttribute("href") === "#" + current) {
+link.classList.add("active");
+}
+
+});
+
+});
+
+/*LOADER HACHER AL INICIAR LA WEB - JS*/
+
+window.addEventListener("load", function(){
+
+setTimeout(function(){
+
+document.getElementById("loader").style.display="none";
+
+},3000);
+
+});
+
+/* CURSOR TECNOLOGICO */
+
+document.addEventListener("DOMContentLoaded", function(){
+
+const cursor = document.querySelector(".cursor");
+
+document.addEventListener("mousemove", function(e){
+
+cursor.style.left = e.clientX + "px";
+cursor.style.top = e.clientY + "px";
+
+});
+
+});
+
+/* EXPERIENCIA ORGANIZACIONES */
+
+const botones = document.querySelectorAll(".cliente-btn");
+
+botones.forEach(boton => {
+
+boton.addEventListener("click", () => {
+
+const info = boton.nextElementSibling;
+
+if(info.style.display === "block"){
+info.style.display = "none";
+}else{
+info.style.display = "block";
+}
+
+});
+
+});
